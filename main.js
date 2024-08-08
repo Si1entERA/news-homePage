81843bd6b9346f3e330d39ba7a90928eabb28a66
Si1entERA <NeoLegote@outlook.com>
2024-08-08 23:44:20 +0200

    grid layout added

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const hamburgerMenu = document.querySelector('.hamburgerMenu');
 const closeSidebar = document.querySelector('.close');
+const overlay = document.getElementById("overlay");
 
 const sidebar = document.querySelector('.sidebar')
 const body = document.body;
@@ -7,11 +8,14 @@ const body = document.body;
 const closeMenu = () => {
   sidebar.classList.remove('open')
   body.classList.remove('lock')
+  overlay.style.display = 'none';
+
 }
 
 const openMenu = () => {
   sidebar.classList.add('open')
   body.classList.add('lock')
+  overlay.style.display = 'block';
 }
 
 closeSidebar.addEventListener('click', closeMenu)
